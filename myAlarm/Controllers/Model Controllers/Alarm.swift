85e9057d3700a882ee696f2e7c10bdaa5c310ee9
Moses <gabe.susman@gmail.com>
2020-04-21 23:50:53 -0600

    first commit

---
+++ myAlarm/Controllers/Model Controllers/Alarm.swift
@@ -0,0 +1,9 @@
+//
+//  Alarm.swift
+//  myAlarm
+//
+//  Created by Moses on 4/20/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import Foundation
---
+++ myAlarm/Controllers/Model Controllers/AlarmController.swift
@@ -0,0 +1,9 @@
+//
+//  AlarmController.swift
+//  myAlarm
+//
+//  Created by Moses on 4/20/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import Foundation
